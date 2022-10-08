3ec3345970efbc591ae16932fa60f6ddaa6999fc
Kunal Thakkar <78646652+kuchbhi-kunal@users.noreply.github.com>
2022-10-08 17:12:18 +0530

    reactiv landing page

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/* to delay the redirecting tof the page
+when user clicks "be reactiv" button so that they can 
+enjoy the cool animation */
+
+function onBtnClickHandle(){
+    setTimeout(function(){ 
+    window.location="reactiv console/reactivconsole.html"
+ }, 1000);
+}
